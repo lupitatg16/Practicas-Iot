45396466ff6b66e29baefc27fd48aefd861340cb
lupitatg16 <lupitatg@iteso.mx>
2024-12-06 15:17:59 -0600

    Updates

---
+++ practica 5/ThingSpeak/temperatureMonitorThingSpeak5_2/secrets.h
@@ -0,0 +1,23 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "CASTOR-PLAY___2.4Gnormal"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "Cas123Tor2022" // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2056226      // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "3NI8QEJLPAVY8GV2"   // replace XYZ with your channel write API Key
+
+#define OPENWEATHER_REQUEST_PATH "https://api.openweathermap.org/data/2.5/weather?lat=20.6024&lon=-103.2714&appid=2a8d176a267afd236aa25106e6f80541&units=metric"
+
+
+/*******************************
+
+#define SECRET_SSID "IoT"   // replace MySSID with your WiFi network name
+#define SECRET_PASS "1t3s0IoT23"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2056226      // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "3NI8QEJLPAVY8GV2"   // replace XYZ with your channel write API Key
+
+#define OPENWEATHER_REQUEST_PATH "https://api.openweathermap.org/data/2.5/weather?lat=20.6024&lon=-103.2714&appid=2a8d176a267afd236aa25106e6f80541&units=metric"
+
+****************************************************************/
